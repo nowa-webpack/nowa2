651f3c47c3f354fcbbb6f4d5332159ba6fa7262b
Tommy Troy Lin <tommytroylin@gmail.com>
2018-01-01 18:11:10 +0800

    fix: should exit on node@6.5- instead of 6-

--- packages/cli/src/bin/nowa.ts
+++ packages/cli/src/bin/nowa.ts
@@ -3,16 +3,9 @@
 
 import run from '../index';
 
-if (Number.parseInt(process.versions.node) < 6) {
-  console.error(new Error('Nowa command needs node 6, please upgrade your environment'));
+if (Number(/^\d+\.\d+/.exec(process.versions.node)) < 6.5) {
+  console.error(`Nowa command needs node@6.5+ but found ${process.versions.node}\nplease upgrade your environment`);
   process.exit(1);
 }
 
-run().then(
-  () => {
-    console.log('Nowa Command Finished');
-  },
-  error => {
-    console.error(error);
-  },
-);
+run().catch(console.error);
