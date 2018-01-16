044790ebd6e766a5382705a974b1b3cd25039af0
Tommy Troy Lin <tommytroylin@gmail.com>
2018-01-16 14:54:26 +0800

    fix: env check failure on node 6.11.x

--- packages/cli/src/bin/nowa.ts
+++ packages/cli/src/bin/nowa.ts
@@ -2,11 +2,12 @@
 'use strict';
 
 import * as importLocal from 'import-local';
+import * as semver from 'semver';
 
 import run from '../index';
 
 if (!importLocal(__filename)) {
-  if (Number(/^\d+\.\d+/.exec(process.versions.node)) < 6.5) {
+  if (semver.lt(process.versions.node, '6.5.0')) {
     console.error(`Nowa@2 needs node@6.5+ but found ${process.versions.node}\nPlease upgrade your environment`);
     process.exit(1);
   }
