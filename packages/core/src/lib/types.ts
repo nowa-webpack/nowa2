04a8b9c0f2430ccdd318b46750cd9d6ee81b8f56
Tommy Troy Lin <tommytroylin@gmail.com>
2018-01-16 20:17:21 +0800

    feat: support prompt option

--- packages/core/src/lib/types.ts
+++ packages/core/src/lib/types.ts
@@ -40,7 +40,7 @@ export interface ISolutionCommandRegistry {
 }
 
 export interface IBaseOptionDescription {
-  type: 'string' | 'number' | 'array' | 'boolean' | 'choice';
+  type: 'string' | 'number' | 'array' | 'boolean' | 'choice' | 'prompt';
   description: string;
   default?: boolean | string | number | string[];
   alias?: string;
@@ -54,6 +54,12 @@ export type IOptionDescription =
   | INumberOptionDescription
   | IArrayOptionDescription
   | IBooleanOptionDescription;
+
+export type IPromptOptionDescription =
+  | ITextPromptOptionDescription
+  | IConfirmPromptOptionDescription
+  | IListPromptOptionDescription
+  | ICheckboxPromptOptionDescription;
 
 export interface IChoiceOptionDescription extends IBaseOptionDescription {
   type: 'choice';
@@ -79,4 +85,46 @@ export interface IBooleanOptionDescription extends IBaseOptionDescription {
 export interface IArrayOptionDescription extends IBaseOptionDescription {
   type: 'array';
   default?: string[];
+}
+
+export interface IBasePromptOptionDescription extends IBaseOptionDescription {
+  type: 'prompt';
+  prompt: 'input' | 'confirm' | 'list' | 'password' | 'checkbox';
+  order: number;
+  filter?: (input: any) => Promise<any> | any;
+  validate?: (result: any) => Promise<any> | any;
+  choices?: any[];
+}
+
+export interface ITextPromptOptionDescription extends IBasePromptOptionDescription {
+  prompt: 'input' | 'password';
+  default: string;
+}
+
+export interface IConfirmPromptOptionDescription extends IBasePromptOptionDescription {
+  prompt: 'confirm';
+  default: boolean;
+}
+export interface IListPromptOptionDescriptionChoices {
+  name: string;
+  value?: string;
+  short?: string;
+}
+
+export interface IListPromptOptionDescription extends IBasePromptOptionDescription {
+  prompt: 'list';
+  choices: Array<IListPromptOptionDescriptionChoices | string>;
+  default: string;
+}
+
+export interface ICheckboxPromptOptionDescriptionChoices {
+  name: string;
+  value?: string;
+  checked?: boolean;
+}
+
+export interface ICheckboxPromptOptionDescription extends IBasePromptOptionDescription {
+  prompt: 'checkbox';
+  choices: Array<IListPromptOptionDescriptionChoices | string>;
+  default: string[];
 }
