08c42f5d3e0596d60b89a063ed05c15c21f5a9d8
Tommy Troy Lin <tommytroylin@gmail.com>
2018-01-18 22:43:25 +0800

    refactor(loadCommands): use utils.logger instead of console.log & debug

--- packages/cli/src/lib/loadCommands.ts
+++ packages/cli/src/lib/loadCommands.ts
@@ -1,13 +1,11 @@
 import { Runner } from '@nowa/core';
-import * as debugLog from 'debug';
 import * as parser from 'yargs-parser';
 
-const debug = debugLog('LoadCommandsPlugin');
-
 export class LoadCommandsPlugin {
-  public apply(runner: Runner) {
+  public apply(runner: Runner, utils: Runner.Utils) {
+    const { logger, chalk } = utils;
     runner.$register('load-commands', async ({ solution }) => {
-      debug(`got argv ${process.argv.join(' ')}`);
+      logger.debug(`got argv ${process.argv.join(' ')}`);
       const commands: string[] = [];
       for (const arg of process.argv.slice(2)) {
         if (arg.startsWith('-')) {
@@ -15,15 +13,15 @@ export class LoadCommandsPlugin {
         }
         commands.push(arg);
       }
-      debug(`got actual argv ${commands.join(' ')}`);
+      logger.debug(`got actual argv ${commands.join(' ')}`);
       const { _ } = parser(commands);
-      debug(`got actual commands ${_.join(' ')}`);
+      logger.debug(`got actual commands ${_.join(' ')}`);
       if (_.length === 0) {
         const helpInfo = solution.help;
         if (helpInfo) {
-          console.log('Available nowa commands:\n');
+          logger.log(`Available nowa commands:\n`);
           Object.keys(helpInfo).forEach(name => {
-            console.log(`${name}\t${helpInfo[name]}`);
+            logger.log(chalk`{blue.bold ${name}}\t${helpInfo[name]}`);
           });
         }
       }
