5486026f7e1ed487a82d4537339a4bfecb91ab4f
Tommy Troy Lin <tommytroylin@gmail.com>
2018-01-18 22:30:16 +0800

    feat: output cli & core version with logo

--- packages/cli/src/bin/nowa.ts
+++ packages/cli/src/bin/nowa.ts
@@ -1,14 +1,21 @@
 #!/usr/bin/env node
 'use strict';
 
+import { resolve } from 'path';
+
 import chalk from 'chalk';
 import * as importLocal from 'import-local';
 import * as isElevated from 'is-elevated';
 import * as semver from 'semver';
 
-import run from '../index';
+import { run } from '../index';
+import logo from '../lib/logo/small';
+
+const { version: cliVersion } = require(resolve(__dirname, '../package.json')); // tslint:disable-line:no-var-requires
+const { version: coreVersion } = require(`@nowa/core/package.json`); // tslint:disable-line:no-var-requires no-submodule-imports
 
 const isWindows = process.platform === 'win32';
+
 isElevated().then(e => {
   if (e) {
     console.log(
@@ -19,7 +26,8 @@ isElevated().then(e => {
     console.log(chalk`{red Nowa needs node @ {bold 6.5+} but found ${process.versions.node}\nPlease upgrade your environment}`);
     process.exitCode = 1;
   } else if (!importLocal(__filename)) {
-    run().catch(e => {
+    console.log(logo(coreVersion, cliVersion));
+    run().catch((e: any) => {
       console.log(chalk`{red ${e.stack ? e.stack : e}}`);
     });
   }
--- packages/cli/src/lib/logo/small.ts
+++ packages/cli/src/lib/logo/small.ts
@@ -1,7 +1,7 @@
 import chalk from 'chalk';
 
-export default chalk`{magenta    _  ______ _      _____
+export default (coreVersion: string, cliVersion: string) => chalk`{magenta    _  ______ _      _____
   / |/ / __ \\ | /| / / _ |
- /    / /_/ / |/ |/ / __ |
-/_/|_/\\____/|__/|__/_/ |_|}
+ /    / /_/ / |/ |/ / __ |\tcore @ {cyan ${coreVersion}}
+/_/|_/\\____/|__/|__/_/ |_|\t cli @ {cyan ${cliVersion}}}
 `;
