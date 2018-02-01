951b5ad57118bf98ba2340ae2537ef186509925f
Tommy Troy Lin <tommytroylin@gmail.com>
2018-02-01 19:06:44 +0800

    refactor: rearrange code

--- packages/cli/src/bin/nowa.ts
+++ packages/cli/src/bin/nowa.ts
@@ -11,24 +11,26 @@ import * as semver from 'semver';
 import { run } from '../index';
 import logo from '../lib/logo/small';
 
-const { version: cliVersion } = require(resolve(__dirname, '../package.json')); // tslint:disable-line:no-var-requires
-const { version: coreVersion } = require(`@nowa/core/package.json`); // tslint:disable-line:no-var-requires no-submodule-imports
-
-const isWindows = process.platform === 'win32';
-
-isElevated().then(e => {
-  if (e) {
-    console.log(
-      chalk`{${isWindows ? 'yellow' : 'red'} Nowa is elevated ${isWindows ? '(administrator)' : '(root privilege)'}\nUse at your own risk}`,
-    );
+if (!importLocal(__filename)) {
+  const { version: cliVersion } = require(resolve(__dirname, '../package.json')); // tslint:disable-line:no-var-requires
+  const { version: coreVersion } = require(`@nowa/core/package.json`); // tslint:disable-line:no-var-requires no-submodule-imports
+  const isWindows: boolean = process.platform === 'win32';
+  const nodeVersion: string = process.versions.node;
+  if (semver.lt(nodeVersion, '6.5.0')) {
+    console.log(chalk`{red Nowa needs node @ {bold 6.5+} but found ${nodeVersion}\nPlease upgrade your environment}`);
+    process.exit(1);
   }
-  if (semver.lt(process.versions.node, '6.5.0')) {
-    console.log(chalk`{red Nowa needs node @ {bold 6.5+} but found ${process.versions.node}\nPlease upgrade your environment}`);
-    process.exitCode = 1;
-  } else if (!importLocal(__filename)) {
-    console.log(logo(coreVersion, cliVersion));
+  console.log(`\n${logo(coreVersion, cliVersion)}`);
+  isElevated().then(e => {
+    if (e) {
+      console.log(
+        chalk`{${isWindows ? 'yellow' : 'red'} Nowa is elevated ${
+          isWindows ? '(administrator)' : '(root privilege)'
+        }\nUse at your own risk}`,
+      );
+    }
     run().catch((e: any) => {
       console.log(chalk`{red ${e.stack ? e.stack : e}}`);
     });
-  }
-});
+  });
+}
