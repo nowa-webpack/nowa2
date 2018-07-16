31783c1c3725dcf87ae2d486646f016a6feb8830
Tommy Troy Lin <tommytroylin@gmail.com>
2018-07-16 19:21:05 +0800

    fix: command path parsing issue

--- packages/core/src/lib/plugins/parseSolution.ts
+++ packages/core/src/lib/plugins/parseSolution.ts
@@ -12,14 +12,17 @@ export class ParseSolutionPlugin {
       if (configResult) {
         logger.debug('using config.commands');
         if (typeof configResult.result === 'function') {
-          return (configResult.result as any)({ params: configResult.params });
+          return (configResult.result as any)({ params: configResult.params }); // TODO: fix any
         }
         return configResult.result;
       }
       const solutionResult = parser('solution.commands', commands, logger.debug, solution.commands);
       if (solutionResult) {
         logger.debug('using solution.commands');
-        return solutionResult;
+        if (typeof solutionResult.result === 'function') {
+          return (solutionResult.result as any)({ params: solutionResult.params }); // TODO: fix any
+        }
+        return solutionResult.result;
       }
       logger.error(`neither of config / solution has described commands.${commands.join('.')}`);
       throw new Error('no correspond command');
