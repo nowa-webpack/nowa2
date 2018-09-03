ba03988b8628bb1883fbd48f2687349503a2c92d
creamidea <creamidea@gmail.com>
2018-09-03 19:21:06 +0800

    feat: add the solution version right the logo

--- packages/cli/src/lib/logo/small.ts
+++ packages/cli/src/lib/logo/small.ts
@@ -1,7 +1,8 @@
 import chalk from 'chalk';
+import { ISolution } from '../SolutionConfiguration';
 
-export default (coreVersion: string, cliVersion: string) => chalk`{magenta    _  ______ _      _____
-  / |/ / __ \\ | /| / / _ |
- /    / /_/ / |/ |/ / __ |\tcore @ {cyan ${coreVersion}}
-/_/|_/\\____/|__/|__/_/ |_|\t cli @ {cyan ${cliVersion}}}
+export default (coreVersion: string, cliVersion: string, solution: ISolution) => chalk`{magenta    _  ______ _      _____
+  / |/ / __ \\ | /| / / _ |\tcore @ {cyan ${coreVersion}}
+ /    / /_/ / |/ |/ / __ |\tcli @ {cyan ${cliVersion}}
+/_/|_/\\____/|__/|__/_/ |_|\t${solution.name} @ {cyan ${solution.version}}}
 `;
