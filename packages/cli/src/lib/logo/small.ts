eb4939b4a394700cc722a48d71c286fb72b50670
Tommy Troy Lin <tommytroylin@gmail.com>
2018-09-14 17:51:48 +0800

    fix: chalk error

--- packages/cli/src/lib/logo/small.ts
+++ packages/cli/src/lib/logo/small.ts
@@ -3,5 +3,5 @@ import chalk from 'chalk';
 export default (coreVersion: string, cliVersion: string) => chalk`{magenta    _  ______ _      _____
   / |/ / __ \\ | /| / / _ |\tcore @ {cyan ${coreVersion}}
  /    / /_/ / |/ |/ / __ |\tcli @ {cyan ${cliVersion}}
-/_/|_/\\____/|__/|__/_/ |_|\t
+/_/|_/\\____/|__/|__/_/ |_|}
 `;
